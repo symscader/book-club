294afbd972a177484feb03200b7e64d7dd6b15d6
symscader <stakaniwe@gmail.com>
2026-08-25 22:45:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BOOK_CLUB_CONFIG = {
-  supabaseUrl: "https://bookclubpinkygirlslovesbooks.supabase.co",
+  supabaseUrl: "https://zkpwstjtxfdyycbgemhw.supabase.co",
   supabaseAnonKey: "sb_publishable_C2lnA5C-FDGU7KosElAwTw_O1Z3d8sv"
 };
